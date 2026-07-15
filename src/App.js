f4dd85bfc0513c33dc92a0a7ee24ff076dabbe32
adir395 <adirmelker1@gmail.com>
2026-07-15 13:59:41 +0300

    sbsb

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemybbvvaa"added new botton"
+          Learn Jenkins on Udemybbvvaa"added new botton test that shit"
         </a>
       </header>
       <p>Application version: 1</p>

231eb5c0b60491218608d9c3ece9c7f9b08bed66
adir395 <adirmelker1@gmail.com>
2026-07-23 18:15:18 +0300

    ששנ

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  fetch("http://192.168.1.1/api/data");
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +12,6 @@ function App() {
           href="https://example.com"
           target="_blank"
           rel="noopener noreferrer"
-          ip_address="192.88.1.1"
         >
           Learn Jenkins on Udemy KING
         </a>
